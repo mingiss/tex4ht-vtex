92d2ca3b108db62b84ea7aee8178600353c27c7e
Mindaugas Piešina <mindaugas.piesina@vtex.lt>
2020-01-27 12:26:43 +0200

    version updated

--- Build/source/texk/tex4htk/tex4ht.c
+++ Build/source/texk/tex4htk/tex4ht.c
@@ -1,5 +1,5 @@
 
-/* tex4ht.c (2020-01-23-12:13), generated from tex4ht-c.tex
+/* tex4ht.c (2020-01-27-10:58), generated from tex4ht-c.tex
    Copyright (C) 2009-2012 TeX Users Group
    Copyright (C) 1996-2009 Eitan M. Gurari
 
@@ -6517,7 +6517,7 @@ CDECL
 #define VTEX_SSCRIPT_ADDONS_SIG ""
 #endif
 
-(IGNORED) printf("tex4ht.c (2020-01-23-12:13%s %dbit%s%s%s%s%s)\n",PLATFORM_SIG, (int)sizeof(void *) * 8, KPATHSEA_SIG, VTEX_ADDONS_SIG, VTEX_SPACING_ADDONS_SIG, VTEX_OTF_ADDONS_SIG, VTEX_SSCRIPT_ADDONS_SIG);
+(IGNORED) printf("tex4ht.c (2020-01-27-10:58%s %dbit%s%s%s%s%s)\n",PLATFORM_SIG, (int)sizeof(void *) * 8, KPATHSEA_SIG, VTEX_ADDONS_SIG, VTEX_SPACING_ADDONS_SIG, VTEX_OTF_ADDONS_SIG, VTEX_SSCRIPT_ADDONS_SIG);
 
 for(i=0; i<argc; i++){
     (IGNORED) printf("%s%s ", (i>1)?"\n  " : "", argv[i]); }
